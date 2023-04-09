25d461abc9230e4e1ec7912b94fffb4cdd88c856
Folasade Ekulona <ekulonaf@gmail.com>
2023-04-09 18:31:15 -0400

    adds completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    return Math.abs(42 - x)
+}
+
+function distanceFromHqInFeet(x){
+    return (distanceFromHqInBlocks(x)* 264);
+}
+
+function distanceTravelledInFeet(start, destination){
+    return (Math.abs(start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination){
+    const fareDistance = distanceTravelledInFeet(start, destination);
+    if (fareDistance < 400){
+        return 0;
+    }
+    else if (fareDistance > 400 && fareDistance <= 2000){
+        return (fareDistance - 400) * 0.02;
+    }
+    else if (fareDistance > 2000 && fareDistance <= 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
